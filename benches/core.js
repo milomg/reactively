7045293059515048d536fe13647da10629348f9d
modderme123 <modderme123@gmail.com>
2022-08-25 22:12:20 -0700

    initial commit

---
+++ benches/core.js
@@ -0,0 +1,130 @@
+let CurrentCompute = undefined;
+
+let effectQueue = [];
+
+export class Reactive {
+  constructor(fnOrValue, effect) {
+    this.state = 2 /* CacheState.DIRTY */;
+    if (typeof fnOrValue === "function") {
+      this.fn = fnOrValue;
+      this.value = undefined;
+      this.effect = effect;
+      if (effect) this.update();
+    } else {
+      this.fn = undefined;
+      this.value = fnOrValue;
+      this.effect = false;
+    }
+    this.sources = null;
+    this.sourceSlots = null;
+    this.observers = null;
+    this.observerSlots = null;
+  }
+  stale(state) {
+    if (this.state < state) this.state = state;
+    if (this.effect) effectQueue.push(this);
+    else if (this.observers) {
+      for (let i = 0; i < this.observers.length; i++) {
+        this.observers[i].stale(1 /* CacheState.CHECK */);
+      }
+    }
+  }
+  set(value) {
+    if (this.value !== value && this.observers) {
+      for (let i = 0; i < this.observers.length; i++) {
+        this.observers[i].stale(2 /* CacheState.DIRTY */);
+      }
+    }
+    this.value = value;
+    for (let i = 0; i < effectQueue.length; i++) {
+      effectQueue[i].get();
+    }
+    effectQueue.length = 0;
+  }
+
+  update() {
+    if (this.sources) {
+      cleanNode(this)
+    }
+    const oldValue = this.value;
+    const listener = CurrentCompute;
+    CurrentCompute = this;
+    try {
+      this.value = this.fn();
+    } finally {
+      CurrentCompute = listener;
+    }
+  
+    this.state = 0 /* CacheState.CURRENT */;
+    if (oldValue !== this.value && this.observers) {
+      for (let i = 0; i < this.observers.length; i++) {
+        this.observers[i].stale(2 /* CacheState.DIRTY */);
+      }
+    }
+  }
+
+  updateIfNecessary() {
+    if (this.state == 2 /* CacheState.DIRTY */) {
+      return this.update();
+    }
+    if (this.state == 1 /* CacheState.CHECK */) {
+      if (this.sources) {
+        for (let i = 0; i< this.sources.length;i++) {
+          this.sources[i].updateIfNecessary();
+        }
+        if (this.state = 2 /* CacheState.DIRTY */) {
+          return this.update();
+        }
+      }
+      this.state = 0 /* CacheState.CURRENT */;
+    }
+  }
+  get() {
+    if (CurrentCompute) {
+      const sSlot = this.observers ? this.observers.length : 0;
+      if (!CurrentCompute.sources) {
+        CurrentCompute.sources = [this];
+        CurrentCompute.sourceSlots = [sSlot];
+      } else {
+        CurrentCompute.sources.push(this);
+        CurrentCompute.sourceSlots.push(sSlot);
+      }
+      if (!this.observers) {
+        this.observers = [CurrentCompute];
+        this.observerSlots = [CurrentCompute.sources.length - 1];
+      } else {
+        this.observers.push(CurrentCompute);
+        this.observerSlots.push(CurrentCompute.sources.length - 1);
+      }
+    }
+    if (this.fn) this.updateIfNecessary();
+    return this.value;
+  }
+}
+
+function cleanNode(node) {
+  while (node.sources.length) {
+    const source = node.sources.pop(),
+      index = node.sourceSlots.pop(),
+      obs = source.observers;
+    if (obs && obs.length) {
+      const n = obs.pop(),
+        s = source.observerSlots.pop();
+      if (index < obs.length) {
+        n.sourceSlots[s] = index;
+        obs[index] = n;
+        source.observerSlots[index] = s;
+      }
+    }
+  }
+}
+
+export function signal(value) {
+  const signal = new Reactive(value);
+  return [signal.get.bind(signal), signal.set.bind(signal)];
+}
+
+export function computed(fn) {
+  const computed = new Reactive(fn, true);
+  return computed.get.bind(computed);
+}
---
+++ benches/grid.js
@@ -0,0 +1,145 @@
+import { Reactive } from "./core.js";
+
+// The following is an implementation of the cellx benchmark https://codesandbox.io/s/cellx-bench-forked-s6kusj
+
+const cellx = (layers) => {
+  const start = {
+    prop1: new Reactive(1),
+    prop2: new Reactive(2),
+    prop3: new Reactive(3),
+    prop4: new Reactive(4),
+  };
+
+  let layer = start;
+
+  for (let i = layers; i > 0; i--) {
+    layer = ((m) => {
+      const s = {
+        prop1: new Reactive(() => m.prop2.get()),
+        prop2: new Reactive(() => m.prop1.get() - m.prop3.get()),
+        prop3: new Reactive(() => m.prop2.get() + m.prop4.get()),
+        prop4: new Reactive(() => m.prop3.get()),
+      };
+
+      s.prop1.get();
+      s.prop2.get();
+      s.prop3.get();
+      s.prop4.get();
+
+      return s;
+    })(layer);
+  }
+
+  const end = layer;
+
+  const startTime = performance.now();
+
+  const before = [
+    end.prop1.get(),
+    end.prop2.get(),
+    end.prop3.get(),
+    end.prop4.get(),
+  ];
+
+  start.prop1.set(4);
+  start.prop2.set(3);
+  start.prop3.set(2);
+  start.prop4.set(1);
+
+  const after = [
+    end.prop1.get(),
+    end.prop2.get(),
+    end.prop3.get(),
+    end.prop4.get(),
+  ];
+
+  const endTime = performance.now();
+  const elapsedTime = endTime - startTime;
+
+  return [elapsedTime, before, after];
+};
+
+const arraysEqual = (a, b) => {
+  if (a.length !== b.length) return false;
+
+  for (var i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+
+  return true;
+};
+
+const benchmark = () => {
+  let total = 0;
+
+  const expected = {
+    10: [
+      [3, 6, 2, -2],
+      [2, 4, -2, -3],
+    ],
+    20: [
+      [2, 4, -1, -6],
+      [-2, 1, -4, -4],
+    ],
+    30: [
+      [-1, -2, -3, -4],
+      [-4, -3, -2, -1],
+    ],
+    50: [
+      [-2, -4, 1, 6],
+      [2, -1, 4, 4],
+    ],
+    100: [
+      [-3, -6, -2, 2],
+      [-2, -4, 2, 3],
+    ],
+    1000: [
+      [-3, -6, -2, 2],
+      [-2, -4, 2, 3],
+    ],
+    2500: [
+      [-3, -6, -2, 2],
+      [-2, -4, 2, 3],
+    ],
+    // 5000: [[2, 4, -1, -6], [-2, 1, -4, -4]]
+  };
+
+  const results = {};
+
+  const runs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  for (const layers in expected) {
+    console.log("----");
+
+    for (const run of runs) {
+      const [elapsed, before, after] = cellx(Number(layers));
+
+      results[layers] = [before, after];
+
+      console.log(`Layers ${layers}: ${elapsed}`);
+
+      total += elapsed;
+    }
+  }
+
+  console.log("----");
+  console.log(`Total: ${total}`);
+
+  for (const layers in expected) {
+    const [before, after] = results[layers];
+    const [expectedBefore, expectedAfter] = expected[layers];
+
+    if (!arraysEqual(before, expectedBefore)) {
+      throw new Error(
+        `Expected first layer ${expectedBefore}, found first layer ${before}`
+      );
+    }
+    if (!arraysEqual(after, expectedAfter)) {
+      throw new Error(
+        `Expected last layer ${expectedAfter}, found last layer ${after}`
+      );
+    }
+  }
+};
+
+benchmark();
