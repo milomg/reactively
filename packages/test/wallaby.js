ed4bec8792fb724574e1830be1fff33abc83ae40
some fool <lee@underneath.ca>
2022-08-29 17:16:59 -0700

    some wallaby experiments, no luck yet

--- packages/test/wallaby.js
+++ packages/test/wallaby.js
@@ -2,10 +2,26 @@ module.exports = () => {
   return {
     autoDetect: true,
     trace: true,
+    debug: true,
     testFramework: {
       configFile: "./jest.config.js",
     },
-    files: ["src/**/*.ts", { pattern: "src/**/*.test.ts", ignore: true }],
+    files: [
+      "src/**/*.ts",
+      // "./../decorate/src/**/*.ts",
+      { pattern: "src/**/*.test.ts", ignore: true },
+    ],
     tests: ["src/**/*.test.ts"],
+    // setup: function (wallaby) {
+    //   const config = require("./jest.config.js");
+    //   Object.keys(config.moduleNameMapper).forEach(
+    //     (k) =>
+    //       (config.moduleNameMapper[k] = config.moduleNameMapper[k].replace(
+    //         "<rootDir>",
+    //         wallaby.localProjectDir
+    //       ))
+    //   );
+    //   wallaby.testFramework.configure(config);
+    // },
   };
 };
