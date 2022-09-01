72b84058fc25043970b4f4c5c55789ab787b5be5
some fool <lee@underneath.ca>
2022-09-01 14:47:34 -0700

    verify number of reactions executed

--- packages/cypress-test/component-test/DynamicDepends.cy.ts
+++ packages/cypress-test/component-test/DynamicDepends.cy.ts
@@ -1,53 +1,67 @@
 import { $r, ReactiveWrap } from "@reactively/wrap";
 
-it("static dependency graph: 10 x 5, 100K iteration", () => {
-  const graph = makeGraph(10, 5);
-
-  const sum = withPerf("sg10x5.100K", () => runGraph(graph, 100000));
-  expect(sum).equals(15999840);
-});
 
 it("static graph", () => {
-  const graph = makeGraph(3, 3);
+  const counter = new Counter();
+  const graph = makeGraph(3, 3, counter);
   const sum = runGraph(graph, 10);
 
   expect(sum).equal(108);
+  expect(counter.count).equals(51);
 });
 
 it("static graph, read 2/3 of leaves", () => {
-  const graph = makeGraph(3, 3);
+  const counter = new Counter();
+  const graph = makeGraph(3, 3, counter);
   const sum = runGraph(graph, 10, 2);
 
   expect(sum).equal(71);
-});
-
-it.only("static graph 10 x 10, read 1/5 of leaves, 100K iterations", () => {
-  const graph = makeGraph(10, 10);
-  const sum = withPerf("sg10x10/5.100K", () => runGraph(graph, 100000, 5));
-  expect(sum).equals(102398976);
+  expect(counter.count).equals(41);
 });
 
 it("dynamic graph", () => {
-  const graph = makeGraph(4, 2, 2);
+  const counter = new Counter();
+  const graph = makeGraph(4, 2, counter, 2);
   const sum = runGraph(graph, 10);
-  // logGraph(graph);
+
   expect(sum).equal(74);
+  expect(counter.count).equal(22);
+});
+
+// perf tests 
+it("static dependency graph: 10 x 5, 100K iteration", () => {
+  const counter = new Counter();
+  const graph = makeGraph(10, 5, counter);
+
+  const sum = withPerf("sg10x5.100K", () => runGraph(graph, 100000));
+  expect(sum).equals(15999840);
+  expect(counter.count).equals(1400026);
+});
+
+it("static graph 10 x 10, read 1/5 of leaves, 100K iterations", () => {
+  const counter = new Counter();
+  const graph = makeGraph(10, 10, counter);
+  const sum = withPerf("sg10x10/5.100K", () => runGraph(graph, 100000, 5));
+  expect(sum).equals(102398976);
+  expect(counter.count).equals(3600036);
 });
 
 it("dynamic graph 10 x 5, 100K iterations", () => {
-  const graph = makeGraph(10, 5, 2);
+  const counter = new Counter();
+  const graph = makeGraph(10, 5, counter, 2);
   const sum = withPerf("dynamicGraph", () => runGraph(graph, 100000));
-  expect(sum).equals(11999955)
+  expect(sum).equals(11999955);
+  expect(counter.count).equals(1250027);
 });
 
-
-/*
-TODO 
-  count executions
-DONE
-  perf test with fewer leaf reads
-  test with fewer leaf reads
-*/
+// TODO perf test with dynamic graph, read only some leaves
+// it("dynamic graph 10 x 10, read 1/5 leaves. 100K iterations", () => {
+//   const counter = new Counter();
+//   const graph = makeGraph(10, 5, counter, 2);
+//   const sum = withPerf("dynamicGraph 10x10 1/5 leaves", () => runGraph(graph, 100000, 5));
+//   expect(sum).equals(2399983);
+//   expect(counter.count).equals(520016);
+// });
 
 function withPerf<T>(name: string, fn: () => T): T {
   const startName = name + ".start";
@@ -71,10 +85,11 @@ function withPerf<T>(name: string, fn: () => T): T {
 function makeGraph(
   width: number,
   layers: number,
+  counter:Counter,
   dynamicNth = 1
 ): ReactiveWrap<number>[][] {
   const sources = new Array(width).fill(0).map((_, i) => $r(i));
-  const rows = makeDependentRows(sources, layers - 1, dynamicNth);
+  const rows = makeDependentRows(sources, layers - 1, counter, dynamicNth);
   return [sources, ...rows];
 }
 
@@ -83,7 +98,11 @@ function makeGraph(
  *
  * @return the sum of all leaf values
  */
-function runGraph(graph: ReactiveWrap<number>[][], iterations: number, readNth = 1): number {
+function runGraph(
+  graph: ReactiveWrap<number>[][],
+  iterations: number,
+  readNth = 1
+): number {
   const sources = graph[0];
   const leaves = graph[graph.length - 1];
   const nthLeaves = leaves.filter((_, i) => i % readNth === 0);
@@ -114,20 +133,26 @@ function logGraph(rows: ReactiveWrap<number>[][]): void {
 function makeDependentRows(
   sources: ReactiveWrap<number>[],
   numRows: number,
+  counter: Counter,
   dynamicNth: number
 ): ReactiveWrap<number>[][] {
   let prevRow = sources;
   const rows = [];
   for (let l = 0; l < numRows; l++) {
-    const row = makeRow(prevRow, dynamicNth);
+    const row = makeRow(prevRow, counter, dynamicNth);
     rows.push(row);
     prevRow = row;
   }
   return rows;
 }
 
+class Counter {
+  count = 0;
+}
+
 function makeRow(
   sources: ReactiveWrap<number>[],
+  counter: Counter,
   dynamicNth = 1
 ): ReactiveWrap<number>[] {
   return sources.map((s, i) => {
@@ -136,10 +161,14 @@ function makeRow(
     const sourceB = sources[sourceIndexB];
     if (i % dynamicNth === 0) {
       // static node, always reference both sources
-      return $r(() => sourceA() + sourceB());
+      return $r(() => {
+        counter.count++;
+        return sourceA() + sourceB();
+      });
     } else {
       // dynamic node, references 2nd source only if 1st source is even
       const node = $r(() => {
+        counter.count++;
         let sum = sourceA() + 1;
         if (sum & 0x1) {
           sum += sourceB();
