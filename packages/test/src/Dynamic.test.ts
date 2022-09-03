14d4c79591a4cceafe39c11e27c48736f5d516a7
lee mighdoll <lee@underneath.ca>
2022-09-03 14:04:00 -0400

    add failing test for dynamic parent reexecution

--- packages/test/src/Dynamic.test.ts
+++ packages/test/src/Dynamic.test.ts
@@ -1,11 +1,16 @@
-import { $r } from "@reactively/wrap";
+import { $r as _ } from "@reactively/wrap";
 
-test("dynamic sources", () => {
-  const a = $r(false);
-  const b = $r(2);
+/*
+a-c
+ %
+b
+*/
+test("dynamic sources recalculate correctly", () => {
+  const a = _(false);
+  const b = _(2);
   let count = 0;
 
-  const c = $r(() => {
+  const c = _(() => {
     count++;
     a() || b();
   });
@@ -19,4 +24,32 @@ test("dynamic sources", () => {
   b.set(4); 
   c();
   expect(count).toBe(2);
+});
+
+/*
+s-a-l
+ \ % 
+  b
+*/
+test.skip("dynamic sources don't re-execute a parent unnecessarily", () => {
+  const s = _(2);
+  const a = _(() => s() + 1);
+  let bCount = 0;
+  const b = _(() => { // b depends on s, so b's always dirty when s changes, but b may be unneeded.
+    bCount++;
+    return s() + 10;
+  });
+  const l = _(() => {
+    let result = a();
+    if (result & 0x1) {
+      result += b(); // only execute b if a is odd
+    }
+    return result;
+  });
+
+  expect(l()).toEqual(15);
+  expect(bCount).toEqual(1);
+  s.set(3);
+  expect(l()).toEqual(4);
+  expect(bCount).toEqual(1); // TODO fixme, should be 1
 });
