5a062e56933e71b27e74ce8ad497fc7dc6ddf9ce
lee mighdoll <lee@underneath.ca>
2022-09-13 21:41:11 -0700

    experimental .value syntax for wrapped reactives
    Co-authored-by: Milo <modderme123@users.noreply.github.com>

--- packages/wrap/src/wrap.ts
+++ packages/wrap/src/wrap.ts
@@ -1,8 +1,11 @@
 import { Reactive } from "@reactively/core";
 
-export interface ReactiveWrap<T> { // LATER rename to something shorter?
+// LATER rename to something shorter?
+export interface ReactiveWrap<T> {
   (): T;
   set(value: T): void;
+  get value(): T;
+  set value(v: T);
 }
 
 export function $r<T>(fn: () => T): ReactiveWrap<T>;
@@ -11,5 +14,17 @@ export function $r<T>(fnOrValue: (() => T) | T): ReactiveWrap<T> {
   const r = new Reactive(fnOrValue);
   const fn = r.get.bind(r) as ReactiveWrap<T>;
   fn.set = r.set.bind(r);
+  Object.defineProperty(fn, "value", {
+    get() {
+      return r.get();
+    },
+    set(v: T) {
+      r.set(v);
+    },
+  });
   return fn;
 }
+
+// CONSIDER would a protoype based approach be faster here?
+// * We could patch the prototype chain to get instance -> set,value -> Function
+// * or use Object.create() (instance w/ set,value) -> Function
