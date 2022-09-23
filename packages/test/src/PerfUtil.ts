708569f1e76719c0a9088b28ec621f87cb3b4b4d
lee mighdoll <lee@underneath.ca>
2022-09-23 16:03:54 -0700

    rm unused

--- packages/test/src/PerfUtil.ts
+++ packages/test/src/PerfUtil.ts
@@ -9,16 +9,3 @@ export function withPerf<T>(name: string, fn: () => T): T {
   console.log(name, "time:", diff);
   return result;
 }
-
-export function graphPerf(
-  graph: Graph,
-  graphName: string,
-  iterations: number,
-  readNth: number,
-  framework: ReactiveFramework
-): number {
-  const perfName = `${graphName} i=${iterations} r=${readNth}`;
-  return withPerf(perfName, () =>
-    runGraph(graph, iterations, readNth, framework)
-  );
-}
