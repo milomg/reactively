82872e1814bdbc9d7497a7ea1bb7b4ffd942bee7
lee mighdoll <lee@underneath.ca>
2022-11-10 18:43:09 -0800

    util for framework generic unit tests

---
+++ packages/test/src/util/FrameworkTest.ts
@@ -0,0 +1,10 @@
+import { ReactiveFramework } from "./ReactiveFramework";
+export function frameworkTest<T>(
+  f: ReactiveFramework,
+  testName: string,
+  fn: () => T
+): void {
+  test(`${f.name} | ${testName}`, () => {
+    f.withBatch(fn);
+  });
+}
