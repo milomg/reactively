135604b2b5682ff94176155c0561381ef583133b
lee mighdoll <lee@underneath.ca>
2022-11-15 13:32:52 -0800

    interim start on fixing cypress perf

--- packages/cypress-test/component-test/Performance.cy.ts
+++ packages/cypress-test/component-test/Performance.cy.ts
@@ -1,22 +1,24 @@
-import { perfTests } from "../../test/src/util/PerfTests";
+import { makePerfList } from "../../test/src/util/AllPerfTests";
 import { withPerf } from "./CypressPerfUtil";
 
-const tests = perfTests({
-  withPerf,
-  collectGarbage: () => {},
-  optimizeFunctionOnNextCall: () => {},
-});
+// const tests = perfTests({
+//   withPerf,
+//   collectGarbage: () => {},
+//   optimizeFunctionOnNextCall: () => {},
+// });
 
-tests.forEach(({ name, run, config, testPullCounts }) => {
-  it(name, () => {
-    const result = run();
-    const { expected } = config;
+const f = makePerfList({});
 
-    if (expected.sum) {
-      expect(result.sum).equals(expected.sum);
-    }
-    if (expected.count && (config.readFraction === 1 || testPullCounts)) {
-      expect(result.count).equals(expected.count);
-    }
-  });
-});
+// tests.forEach(({ name, run, config, testPullCounts }) => {
+//   it(name, () => {
+//     const result = run();
+//     const { expected } = config;
+
+//     if (expected.sum) {
+//       expect(result.sum).equals(expected.sum);
+//     }
+//     if (expected.count && (config.readFraction === 1 || testPullCounts)) {
+//       expect(result.count).equals(expected.count);
+//     }
+//   });
+// });
--- packages/test/src/util/PerfUtil.ts
+++ packages/test/src/util/PerfUtil.ts
@@ -1,17 +1,9 @@
-/** run a function, logging how long it takes */
-export function withPerfLog<T>(name: string, fn: () => T): T {
-  const start = performance.now();
-  const result = fn();
-  const diff = performance.now() - start;
-  console.log(name, "time:", diff);
-  return result;
-}
-
 export interface TimedResult<T> {
   result: T;
   time: number;
 }
 
+/** run a function, recording how long it takes */
 export function runTimed<T>(fn: () => T): TimedResult<T> {
   const start = performance.now();
   const result = fn();
